82eb530a6900ac7381fe26c161b8c3de9cf50523
Nipa <knowledgezone404@gmail.com>
2022-07-20 14:03:37 +0600

    courseslider and footer add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CoursesAutoSlider from './CoursesAutoSlider';
 
 const Home = () => {
     return (
         <div>
-            
+            <CoursesAutoSlider></CoursesAutoSlider>
         </div>
     );
 };
