a87c19cbe8ff68d8176854cb92ce01ace4368e19
BloomingGang <knowledgezone404@gmail.com>
2022-07-20 11:38:07 +0600

    update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,9 @@ function App() {
       <h1>Faisal Karim</h1>
       <h1>developer</h1>
       <h1>hello this is saddam</h1>
-      <h1>hello this is don</h1>
-      <h1>hello this is Ok</h1>
-
-
-
       <h1>Hello Developer</h1>
-<h1>Hello everyone....</h1>
-<h1>Nipa Branch Create</h1>
-
+      <h1>Hello everyone....</h1>
+      <h1>Nipa Branch Create</h1>
     </div>
   );
 }
