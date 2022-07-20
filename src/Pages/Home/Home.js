5d13d5e3865221dd1e0e4c260aa46872a89b98c4
BloomingGang <knowledgezone404@gmail.com>
2022-07-20 12:35:03 +0600

    added nevbar & hero banner

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import HeroBanner from "../HeroBanner";
+import Navbar from "../Home/Navbar";
 import CardK from "./CardK";
 import Reviews from "./Reviews";
-import Navbar from "../Home/Navbar";
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <HeroBanner/>
       <CardK></CardK>
       <Reviews></Reviews>
     </div>
