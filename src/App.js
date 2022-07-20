ae42fdb331c9a7e75dcc72b54a97cb1d0e41bba5
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-20 14:11:56 +0600

    lerarning program segment procedure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
+import GradeFourToEight from "./Pages/Routes/LearningProgram/GradeFourToEight";
+import GradeNineToTwelve from "./Pages/Routes/LearningProgram/GradeNineToTwelve";
+import GradeThree from "./Pages/Routes/LearningProgram/GradeThree";
+import LearningProgram from "./Pages/Routes/LearningProgram/LearningProgram";
 
 function App() {
   return (
     <div>
-
+           <Routes>
+           <Route path="/home" element={<LearningProgram/>} >
+          <Route path="gradeThree" element={<GradeThree/>}/>
+          <Route path="gradeEight" element={<GradeFourToEight/>}/>
+          <Route path="gradeTwelve" element={<GradeNineToTwelve/>}/>
+        </Route>
+           </Routes>
     </div>
   );
 }
