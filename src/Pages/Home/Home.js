5821ad778f297c3ca295acede1448ebddc1ffba9
faisalkn-71 <csust2021@gmail.com>
2022-07-20 12:07:48 +0600

    EduBanner-faisal

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CardK from "./CardK";
+import EduBanner from "./EduBanner";
 import Reviews from "./Reviews";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <CardK></CardK>
       <Reviews></Reviews>
+      <EduBanner></EduBanner>
     </div>
   );
 };
