770590a7597947b2c3417f006443af224c687462
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-28 22:48:22 +0600

    class six  section complete

--- src/Pages/Routes/ClassOneToTwelve/ClassSix.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassSix.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassesCart from './ClassesCart';
 
 const ClassSix = () => {
+    const [classSix, setClassSix] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/classSix")
+            .then(res => res.json())
+            .then(data => setClassSix(data))
+    }, [])
     return (
-        <div>
-            class six
+        <div className='grid grid-cols-4 gap-4 px-5'>
+            {
+                classSix?.map((course) => <ClassesCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </ClassesCart>)
+            }
         </div>
     );
 };
