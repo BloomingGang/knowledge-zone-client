9a0d8a7db51d71f4a286ce55307e9b5d071c83e7
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-28 22:56:50 +0600

    class eight  section complete

--- src/Pages/Routes/ClassOneToTwelve/ClassEight.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassEight.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassesCart from './ClassesCart';
 
 const ClassEight = () => {
+    const [classEight, setClassEight] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/classEight")
+            .then(res => res.json())
+            .then(data => setClassEight(data))
+    }, [])
     return (
-        <div>
-           class eight 
+        <div className='grid grid-cols-4 gap-4 px-5'>
+            {
+                classEight?.map((course) => <ClassesCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </ClassesCart>)
+            }
         </div>
     );
-};
-
+        }
 export default ClassEight;
