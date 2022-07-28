b885fe8b8c510058be0498e22b8df75ef56b9b18
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-28 22:45:40 +0600

    classfive  section complete

--- src/Pages/Routes/ClassOneToTwelve/ClassFIve.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassFIve.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassesCart from './ClassesCart';
 
 const ClassFIve = () => {
+    const [classFive, setClassFive] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/classFive")
+            .then(res => res.json())
+            .then(data => setClassFive(data))
+    }, [])
     return (
-        <div>
-            class five
+        <div className='grid grid-cols-4 gap-4 px-5'>
+            {
+                classFive?.map((course) => <ClassesCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </ClassesCart>)
+            }
         </div>
     );
 };
