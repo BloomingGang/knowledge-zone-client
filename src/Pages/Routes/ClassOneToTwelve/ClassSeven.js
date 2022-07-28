73c55048b1a3d3e65a09e2bf15f1419d7502d5f1
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-28 22:52:26 +0600

    class seven  section complete

--- src/Pages/Routes/ClassOneToTwelve/ClassSeven.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassSeven.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassesCart from './ClassesCart';
 
 const ClassSeven = () => {
+    const [classSeven, setClassSeven] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/classSeven")
+            .then(res => res.json())
+            .then(data => setClassSeven(data))
+    }, [])
     return (
-        <div>
-           class seven 
+        <div className='grid grid-cols-4 gap-4 px-5'>
+            {
+                classSeven?.map((course) => <ClassesCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </ClassesCart>)
+            }
         </div>
     );
 };
