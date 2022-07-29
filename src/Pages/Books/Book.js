a7d8ec84e58f21e98785f47ad795645b8037f306
kausarahamed <galibhasan517@gmail.com>
2022-07-29 10:35:14 +0600

    kausar

--- src/Pages/Books/Book.js
+++ src/Pages/Books/Book.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 const Book = ({ book }) => {
-  const { description, userName } = book;
+  const { description, userName, img } = book;
   return (
     <div>
       <div class="card w-96 bg-base-100 shadow-xl">
         <figure class="px-10 pt-10">
-          <img
-            src="https://placeimg.com/400/225/arch"
-            alt="Shoes"
-            class="rounded-xl"
-          />
+          <img src={img} alt="Shoes" class="rounded-xl" />
         </figure>
         <div class="card-body items-center text-center">
           <h2 class="card-title">{userName}</h2>
