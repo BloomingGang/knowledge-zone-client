615b8b7d92855282ebdd4397f3d9c593e366b6f0
Nipa <knowledgezone404@gmail.com>
2022-08-03 23:48:47 +0600

    instructor section update

--- src/Pages/Routes/Instructor/Chemistry.js
+++ src/Pages/Routes/Instructor/Chemistry.js
@@ -6,31 +6,26 @@ const Chemistry = () => {
             img:'https://i.ibb.co/5K8Jrcy/20210309-143715.jpg',
             name:'Faysal Karim',
             education: 'Bsc. in Chemistry, DU',
-            speech:'The expert in anything was once a beginner'
         },
         {
             img:'https://i.ibb.co/QcXxxGJ/me.jpg',
             name:'Hossain Rafi',
             education: 'Bsc. in Chemistry, JU',
-            speech:'The expert in anything was once a beginner'
         },
         {
             img:'https://i.ibb.co/k0VGsrX/294706853-594656798735676-1883816395034222539-n-removebg-preview.png',
             name:'Rakib Mia',
             education: 'Bsc. in Chemistry, RU',
-            speech:'The expert in anything was once a beginner'
         },
         {
             img:'https://i.ibb.co/pjYWSz5/293666536-410740174208712-4704341600776933626-n.png',
             name:'Kausar Ahamed',
             education: 'Bsc. in Chemistry, BU',
-            speech:'The expert in anything was once a beginner'
         },
         {
             img:' https://i.ibb.co/PzND9YF/288865092-1499923620424923-4773973340299341148-n.png',
             name:'Saddam Hossain',
             education: 'Bsc. in Chemistry, CU',
-            speech:'The expert in anything was once a beginner'
         }
     ]
     return (
@@ -43,10 +38,14 @@ const Chemistry = () => {
                   <img style={{height: "300px"}} src={instructor.img} alt="instructor" className="rounded-xl " />
                 </figure>
                 <div className="card-body items-center text-center">
-                  <h2 className="card-title">{instructor.name}</h2>
+                  <h2 className="card-title text-primary">{instructor.name}</h2>
                   <h3>{instructor.education}</h3>
-                  <p>{instructor.speech}</p>
                 </div>
+                <div className='text-2xl pb-4 sm:text-center'>
+                        <a href="" target={'_blank'}><i className="fa-brands fa-facebook-square mr-4"></i> </a>
+                        <a href="" target={'_blank'} rel='noreferrer'><i className="fa-brands fa-twitter-square mr-4"></i> </a>
+                        <a href="" target={'_blank'} rel='noreferrer'><i className="fa-brands fa-youtube-square"></i></a>
+                    </div>
               </div>) 
         }
         
