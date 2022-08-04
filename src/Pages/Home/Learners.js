f910d84668c45d2f08241b3f2b7c5fee666a60c4
HossainRafi <hossainrafi71@gmail.com>
2022-08-04 20:43:00 +0600

    update

--- src/Pages/Home/Learners.js
+++ src/Pages/Home/Learners.js
@@ -8,39 +8,39 @@ const Learners = () => {
   return (
     <div className="w-10/12 mx-auto shadow-xl py-6 md:mt-[-50px] bg-white rounded-xl">
       <h1 className="text-center md:text-4xl text-2xl text-primary font-bold xl mb-10">
-        Trusted by 150 million+ learners
+        Trusted By 150 Million+ Learners
       </h1>
-      <div className="md:flex justify-evenly mb-4">
-        <div>
+      <div className="md:flex justify-evenly">
+        <div className="mb-8">
           <div className="flex justify-center">
-            <img src={trust1} alt="" />
+            <img src={trust1} alt="" className="h-[85px] w-[85px]" />
           </div>
           <p className="font-bold text-center">
-            Live and <br /> personalised
+            Live and <br /> Personalised
+          </p>
+        </div>
+        <div className="mb-8">
+          <div className="flex justify-center">
+            <img src={trust2} alt="" className="h-[85px] w-[85px]" />
+          </div>
+          <p className="font-bold text-center">
+            Top Qualified <br /> Teachers
+          </p>
+        </div>
+        <div className="mb-8">
+          <div className="flex justify-center">
+            <img src={trust3} alt="" className="h-[85px] w-[85px]" />
+          </div>
+          <p className="font-bold text-center">
+            Activity-based <br /> Learning
           </p>
         </div>
         <div>
           <div className="flex justify-center">
-            <img src={trust2} alt="" />
+            <img src={trust4} alt="" className="h-[85px] w-[85px]" />
           </div>
           <p className="font-bold text-center">
-            Top qualified <br /> teachers
-          </p>
-        </div>
-        <div>
-          <div className="flex justify-center">
-            <img src={trust3} alt="" />
-          </div>
-          <p className="font-bold text-center">
-            Activity-based <br /> learning
-          </p>
-        </div>
-        <div>
-          <div className="flex justify-center">
-            <img src={trust4} alt="" />
-          </div>
-          <p className="font-bold text-center">
-            100% safe for <br /> kids
+            100% Safe For <br /> Kids
           </p>
         </div>
       </div>
