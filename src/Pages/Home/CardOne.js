95a54fedb44b1d313b6e00aaf93e45df4997caaa
HossainRafi <hossainrafi71@gmail.com>
2022-08-04 21:52:53 +0600

    logo change

--- src/Pages/Home/CardOne.js
+++ src/Pages/Home/CardOne.js
@@ -2,9 +2,9 @@ import React from "react";
 import img1 from "../../img/assets/card1.png";
 import img2 from "../../img/assets/card2.png";
 import img3 from "../../img/assets/card3.png";
-import logo1 from "../../img/assets/cnn-logo.png";
-import logo2 from "../../img/assets/forbs-logo.png";
-import logo3 from "../../img/assets/wef-logo.png";
+import logo1 from "../../img/assets/tds.png";
+import logo2 from "../../img/assets/pa.png";
+import logo3 from "../../img/assets/bbc.png";
 
 const CardOne = () => {
   return (
@@ -12,7 +12,7 @@ const CardOne = () => {
       <div className="container mx-auto">
       <div className="grid md:grid-cols-3 gap-10 py-16 ">
         <div className="bg-white rounded-xl p-5">
-          <img src={logo1} alt="" className="w-[100px] h-[80px] pb-5" />
+          <img src={logo1} alt="" className="w-[115px] h-[80px] pb-5" />
           <img src={img1} alt="" className="rounded-lg pb-5" />
           <p className="text-lg text-justify">
             Bangalore-based education technology company Byju's became India's
@@ -21,7 +21,7 @@ const CardOne = () => {
           </p>
         </div>
         <div className="bg-white rounded-xl p-5">
-          <img src={logo2} alt="" className="w-[100px] h-[80px] pb-5" />
+          <img src={logo2} alt="" className="w-[115px] h-[80px] pb-5" />
           <img src={img2} alt="" className="rounded-lg pb-5" />
           <p className="text-lg text-justify">
             Bangalore-based education technology company Byju's became India's
@@ -30,7 +30,7 @@ const CardOne = () => {
           </p>
         </div>
         <div className="bg-white rounded-xl p-5">
-          <img src={logo3} alt="" className="w-[100px] h-[80px] pb-5" />
+          <img src={logo3} alt="" className="w-[115px] h-[80px] pb-5" />
           <img src={img3} alt="" className="rounded-lg pb-5" />
           <p className="text-lg text-justify">
             Bangalore-based education technology company Byju's became India's
