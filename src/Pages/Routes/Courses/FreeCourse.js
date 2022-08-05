a76efda646b09f4fe128b85993ef0cef84b4624a
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-08-05 11:20:21 +0600

    modify islamic course

--- src/Pages/Routes/Courses/FreeCourse.js
+++ src/Pages/Routes/Courses/FreeCourse.js
@@ -10,40 +10,7 @@ const FreeCourse = () => {
             .then(data => setFreeCourses(data))
     }, [])
 
-   const v= [
-        {
-            
-            "img": "https://i.ibb.co/D4Nw56x/discount-Course.jpg",
-            "title": "Buy this course",
-            "price": 50
-
-        },
-        {
-            
-            "img": "https://i.ibb.co/D4Nw56x/discount-Course.jpg",
-            "title": "Buy this course",
-            "price": 100
-
-        },
-        {
-            
-            "img": "https://i.ibb.co/D4Nw56x/discount-Course.jpg",
-            "title": "Buy this course",
-            "price": 150
-
-        },
-        {
-            
-            "img": "https://i.ibb.co/D4Nw56x/discount-Course.jpg",
-            "title": "Buy this course",
-            "price": 150
-
-        }
-        
-    ]
-
-
-
+   
     return (
         <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  gap-4 mx-5'>
             {
--- src/Pages/Routes/Courses/IslamicCourse.js
+++ src/Pages/Routes/Courses/IslamicCourse.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react';
 import CourseCart from './CourseCart';
 
 const IslamicCourse = () => {
-    
+    const [islamicCourse, setIslamicCourse] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/islamicCourse")
+            .then(res => res.json())
+            .then(data => setIslamicCourse(data))
+    }, [])
 
 
     return (
         <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  gap-4 mx-5'>
             {
-                discountCourse?.map((course) => <CourseCart
+                islamicCourse?.map((course) => <CourseCart
                     key={course._id}
                     course={course}
                 >
