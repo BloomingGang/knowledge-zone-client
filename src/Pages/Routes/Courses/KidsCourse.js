f3c7cb509361d889b7b414c7d461868d9cc64fe7
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-08-05 11:35:51 +0600

    modify kids course routes

--- src/Pages/Routes/Courses/KidsCourse.js
+++ src/Pages/Routes/Courses/KidsCourse.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CourseCart from './CourseCart';
 
 const KidsCourse = () => {
+    const [kidsCourse, setKidsCourse] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/kidsCourse")
+            .then(res => res.json())
+            .then(data => setKidsCourse(data))
+    }, [])
+
+
     return (
-        <div>
-            kids course
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  gap-4 mx-5'>
+            {
+                kidsCourse?.map((course) => <CourseCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </CourseCart>)
+            }
         </div>
     );
 };
