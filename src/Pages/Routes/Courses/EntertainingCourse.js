4d6af012fb65b2caa47fac5c61a34a79d1d587d1
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-08-05 11:39:13 +0600

    modify entertainment routes

--- src/Pages/Routes/Courses/EntertainingCourse.js
+++ src/Pages/Routes/Courses/EntertainingCourse.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CourseCart from './CourseCart';
 
 const EntertainingCourse = () => {
+    const [entertainCourse, setEntertainCourse] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/entertainCourse")
+            .then(res => res.json())
+            .then(data => setEntertainCourse(data))
+    }, [])
+
+
     return (
-        <div>
-            Entertaining Course
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  gap-4 mx-5'>
+            {
+                entertainCourse?.map((course) => <CourseCart
+                    key={course._id}
+                    course={course}
+                >
+
+                </CourseCart>)
+            }
         </div>
     );
 };
