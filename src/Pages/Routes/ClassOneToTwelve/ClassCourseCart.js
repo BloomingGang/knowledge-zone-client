3c753dec2b2d3e4c631540ae1e0e95790e995f73
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-08-11 18:50:17 +0600

    classcoursecard update

--- src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
@@ -7,6 +7,7 @@ const ClassCourseCart = ({course}) => {
     const handleCourse = id => {
       navigate(`/${classCourse}/coursesInfo/${id}`);
   }
+  
    
     return (
             <section>
@@ -14,14 +15,22 @@ const ClassCourseCart = ({course}) => {
                 <figure><img src={img} alt="course" /></figure>
                 <div className="card-body">
                     <h2 className="card-title"> {title}</h2>
-                    <div className="divider"></div>
+                    <hr  className='border-2'/>
                     <div className="flex  justify-between items-center">
                         <span > <span className='text-xl font-bold'>Price:</span>  <span className='text-lg font-bold text-primary'>${price}</span></span>
                         <button 
-                        className="btn btn-primary"
+                        className="border-2 shadow-lg hover:bg-transparent hover:text-black p-2 rounded-lg bg-primary text-white font-bold transition duration-300"
                         onClick={()=>handleCourse(_id)}
                         >Enroll Now</button>
                     </div>
+                   <div>
+                   <hr  className='border-2 mb-3'/>
+                    <div className='flex justify-between'>
+                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Add</button>
+                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Delete</button>
+                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Update</button>
+                    </div>
+                   </div>
                 </div>
             </div>
         </section>
