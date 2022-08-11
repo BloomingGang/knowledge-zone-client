ec5a2efb95a3084c1814f4f4852d7f1a76739587
saddamhosan <contact.saddamhosan@gmail.com>
2022-08-11 12:49:49 +0600

    update payment

--- src/payment/Payment.js
+++ src/payment/Payment.js
@@ -25,20 +25,21 @@ const Payment = () => {
     if(isLoading){
         return <Loading/>
     }
-    const {title,price,img}=course
+    console.log(course);
+    const { productName, price, img } = course;
     return (
       <div className=" w-3/5 mx-auto min-h-screen my-20">
         <div className=" bg-base-100 shadow-xl text-center font-serif py-4">
           <p className="font-bold text-blue-500">Hello, {user.displayName}</p>
           <h2 className=" text-center text-2xl font-bold">
-            Please pay for {title}
+            Please pay for {productName}
           </h2>
           <div class="avatar my-4">
             <div class="w-24 mask mask-hexagon flex justify-center items-center">
               <img src={img} alt="" />
             </div>
           </div>
-          <p>Please Pay : ${price}</p>
+          <p>Please Pay : $ {price}</p>
         </div>
         <div className="card bg-base-100 shadow-xl my-12">
           <div className="card-body">
