311b4ae6a3e23e598cabf3f91b632999a3954eee
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-08-12 09:43:19 +0600

    add new enroll and delete segment

--- src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
@@ -1,36 +1,68 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import useAdmin from '../../../hooks/useAdmin';
+import { TrashIcon,PlusIcon,PencilAltIcon } from "@heroicons/react/solid";
 
-const ClassCourseCart = ({course}) => {
-    const {price,title,img,_id,classCourse}=course
+const ClassCourseCart = ({ course }) => {
+    const { price, title, img, _id, classCourse } = course
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     const navigate = useNavigate();
     const handleCourse = id => {
-      navigate(`/${classCourse}/coursesInfo/${id}`);
-  }
-  
-   
+        navigate(`/${classCourse}/coursesInfo/${id}`);
+    }
+    const handleDeleteCourse =id=>{
+        const proceed = window.confirm("Are you sure?");
+        if (proceed) {
+          const url = `http://localhost:5000/course/${id}`;
+          fetch(url, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              window.location.reload();
+            });
+        }
+        
+    }
+
+
     return (
-            <section>
+        <section>
             <div className="card card-compact  bg-base-100 shadow-xl">
                 <figure><img src={img} alt="course" /></figure>
                 <div className="card-body">
                     <h2 className="card-title"> {title}</h2>
-                    <hr  className='border-2'/>
+                    <hr className='border-2' />
                     <div className="flex  justify-between items-center">
                         <span > <span className='text-xl font-bold'>Price:</span>  <span className='text-lg font-bold text-primary'>${price}</span></span>
-                        <button 
-                        className="border-2 shadow-lg hover:bg-transparent hover:text-black p-2 rounded-lg bg-primary text-white font-bold transition duration-300"
-                        onClick={()=>handleCourse(_id)}
+                        <button
+                            className="border-2 shadow-lg hover:bg-transparent hover:text-black p-2 rounded-lg bg-primary text-white font-bold transition duration-300"
+                            onClick={() => handleCourse(_id)}
                         >Enroll Now</button>
                     </div>
-                   <div>
-                   <hr  className='border-2 mb-3'/>
-                    <div className='flex justify-between'>
-                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Add</button>
-                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Delete</button>
-                        <button className='bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>Update</button>
-                    </div>
-                   </div>
+                    
+                    {
+                        admin && <div>
+                            <hr className='border-2 mb-3' />
+                            <div className='flex justify-between'>
+                                <button 
+                                className='text-center flex bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'
+                                > <PlusIcon className='h-5 text-center w-5'></PlusIcon> Add</button>
+                                <button
+                                onClick={()=>handleDeleteCourse(_id)}
+                                 className='text-center flex bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'
+                                 > <TrashIcon className='h-5 text-center w-5'/> Delete</button>
+                                <button 
+                                className='text-center flex bg-[#7d16eb] border-2 border-gray-700 rounded-lg px-2 py-2 hover:bg-transparent text-white hover:text-black transition duration-300'>
+                                    <PencilAltIcon className='h-5 text-center w-5'/>
+                                    Update</button>
+                            </div>
+                        </div>
+                    }
+
                 </div>
             </div>
         </section>
