5bc4ddc85646db073d6af32b8517a596a348308d
HossainRafi <hossainrafi71@gmail.com>
2022-08-17 19:00:10 +0600

    done

--- src/Pages/Shared/BlogInfo/BlogInfo.js
+++ src/Pages/Shared/BlogInfo/BlogInfo.js
@@ -15,17 +15,18 @@ const BlogInfo = () => {
   }, [id]);
   const { description, writerName, img, date, title } = blog;
   return (
-    <div className="px-16 py-10">
-      <img className="mb-5 w-full h-[85vh]" src={img} alt="" />
-      <h1 className="text-5xl font-bold font-mono text-justify text-indigo-600">
+    <div className="p-8">
+      <img className="mb-5 w-full lg:h-[85vh]" src={img} alt="" />
+      <h1 className="text-3xl md:text-5xl font-bold font-mono text-justify text-indigo-600">
         {title}
       </h1>
-      <div className="pt-3 pb-8 text-3xl font-semibold text-gray-600">
+      <div className="pt-3 text-3xl font-semibold text-gray-600">
         <span>This blog is posted on </span>
         <span className="text-red-500">{date} </span> <span>By </span>
         <span className="text-indigo-600">{writerName}</span>
       </div>
-      <p className="text-2xl text-justify leading-10">{description}</p>
+      <div class="divider"></div>
+      <p className="text-2xl text-justify leading-10 pt-3 px-5">{description}</p>
     </div>
   );
 };
