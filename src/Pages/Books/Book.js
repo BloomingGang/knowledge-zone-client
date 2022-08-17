06b6ef72e779ee4a09201e85fca33303cecf150a
HossainRafi <hossainrafi71@gmail.com>
2022-08-17 22:08:54 +0600

    transition added

--- src/Pages/Books/Book.js
+++ src/Pages/Books/Book.js
@@ -9,7 +9,7 @@ const Book = ({ book, setModal }) => {
   };
   return (
     <div>
-      <div className="card bg-gray-100 h-[600px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="card bg-gray-100 h-[600px] transition ease-in-out delay-150 hover:-translate-1 hover:scale-105 duration-300">
         <div className="">
           <img src={img} alt="/" className="rounded-t-xl h-[380px] w-full" />
         </div>
--- src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
+++ src/Pages/Routes/ClassOneToTwelve/ClassCourseCart.js
@@ -34,7 +34,7 @@ const ClassCourseCart = ({ course }) => {
 
   return (
     <section>
-      <div className="card card-compact bg-gray-100 ">
+      <div className="card card-compact bg-gray-100 transition ease-in-out delay-150 hover:-translate-1 hover:scale-105 duration-300">
         <figure>
           <img
             className="hover:scale-[1.1] transition duration-300"
