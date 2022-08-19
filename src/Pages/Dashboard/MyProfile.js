06c321533f6423c0f88db247e28d25ed2a7c180d
HossainRafi <hossainrafi71@gmail.com>
2022-08-19 11:28:38 +0600

    incomplete update profile

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useEffect } from "react";
 import auth from "../../firebase.init";
 import ProfileUpdate from "./ProfileUpdate";
+import { PencilAltIcon } from "@heroicons/react/solid";
 
 const MyProfile = () => {
   const [user] = useAuthState(auth);
@@ -19,45 +20,56 @@ const MyProfile = () => {
 
   const { education, location, phone } = profile;
   return (
-    <div className="bg-gray-200">
-      <h1 className="text-center pt-10 font-serif font-bold text-4xl text-violet-800">
-        My Profile
-      </h1>
-      <div className="lg:flex lg:flex-row-reverse justify-center w-full px-5 py-8">
-        
-        <div class="avatar">
-          <div class="w-64 h-64 rounded-full">
-            <img src={photoUrl} alt="" />
-          </div>
+    <div className="bg-violet-100">
+      <div className="md:w-2/3 md:mx-auto bg-violet-200">
+        <div className="flex justify-between  items-center">
+          <h1 className="text-center pt-10 font-serif font-bold text-4xl text-violet-800">
+            My Profile
+          </h1>
+          <span>
+            <label
+              for="profileUpdate"
+              onClick={() => setShowProfile(true)}
+              className="text-green-600 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300 mt-4 mx-2"
+            >
+              <PencilAltIcon className="h-9 w-9" />
+            </label>
+          </span>
         </div>
+        <div className="lg:flex lg:flex-row-reverse justify-center w-full px-5 py-8">
+          <div class="avatar">
+            <div class="w-64 h-64 rounded-full">
+              <img src={photoUrl} alt="" />
+            </div>
+          </div>
 
-        <div className="lg:w-3/5 pl-16 md:pt-20 pt-24">
-          <p className="text-3xl py-1 font-semibold pt-5">
-            Name: <span className="text-3xl font-normal pl-3">{name}</span>
-          </p>
-          <p className="text-3xl py-1 font-semibold pt-5">
-            Email: <span className="text-3xl font-normal pl-3">{email}</span>
-          </p>
-          <p className="text-3xl py-1 font-semibold pt-5">
-            Education:{" "}
-            <span className="text-3xl font-normal pl-3">
-              {education || "Not found"}
-            </span>
-          </p>
-          <p className="text-3xl py-1 font-semibold pt-5">
-            Location:{" "}
-            <span className="text-3xl font-normal pl-3">
-              {location || "Not found"}
-            </span>
-          </p>
-          <p className="text-3xl py-1 font-semibold pt-5">
-            Phone:{" "}
-            <span className="text-3xl font-normal pl-3">
-              {" "}
-              {phone || "Not found"}
-            </span>
-          </p>
-          <div className="card-actions justify-center">
+          <div className="lg:w-3/5 pl-16 md:pt-20 pt-24">
+            <p className="text-3xl py-1 font-semibold pt-5">
+              Name: <span className="text-3xl font-normal pl-3">{name}</span>
+            </p>
+            <p className="text-3xl py-1 font-semibold pt-5">
+              Email: <span className="text-3xl font-normal pl-3">{email}</span>
+            </p>
+            <p className="text-3xl py-1 font-semibold pt-5">
+              Education:{" "}
+              <span className="text-3xl font-normal pl-3">
+                {education || "Not found"}
+              </span>
+            </p>
+            <p className="text-3xl py-1 font-semibold pt-5">
+              Location:{" "}
+              <span className="text-3xl font-normal pl-3">
+                {location || "Not found"}
+              </span>
+            </p>
+            <p className="text-3xl py-1 font-semibold pt-5">
+              Phone:{" "}
+              <span className="text-3xl font-normal pl-3">
+                {" "}
+                {phone || "Not found"}
+              </span>
+            </p>
+            {/* <div className="card-actions justify-center">
             <label
               for="profileUpdate"
               onClick={() => setShowProfile(true)}
@@ -65,12 +77,17 @@ const MyProfile = () => {
             >
               UPDATE YOUR PROFILE
             </label>
+          </div> */}
           </div>
         </div>
-
-
+        {showProfile && (
+          <ProfileUpdate
+            setShowProfile={setShowProfile}
+            name={name}
+            email={email}
+          />
+        )}
       </div>
-      {showProfile && <ProfileUpdate setShowProfile={setShowProfile} name={name} email={email} />}
     </div>
   );
 };
